3dde237bf06cacf57bc4c107f79a749c9a5b3fe5
sss2013 <kshi9478@gmail.com>
2024-09-29 20:14:06 +0900

    Web 복습 CSS 기본까지

---
+++ src/main/kotlin/com/example/webpractice/WebPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.webpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebPracticeApplication>(*args)
+}
